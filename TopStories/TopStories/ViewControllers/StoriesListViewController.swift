25683ee5d9993a90bc27a3c2879cb14063c97c5b
Ghulam Nasir <nasirky@gmail.com>
2018-08-24 00:34:39 +0200

    Basic Setup

---
+++ TopStories/TopStories/ViewControllers/StoriesListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ListViewController.swift
+//  TopStories
+//
+//  Created by Ghulam Nasir.
+//  Copyright © 2018 Ghulam Nasir. All rights reserved.
+//
+
+import UIKit
+
+class StoriesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
